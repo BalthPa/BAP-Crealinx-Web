7f1222ed49c135cfb031f839afcd028620948f83
BalthPa <balthazarpapin@gmail.com>
2019-12-19 12:10:19 +0100

    Createur lecteur

---
+++ pages/lecteur.php
@@ -0,0 +1,27 @@
+<?php include ('../inc/header.inc.php') ?>
+
+<div class='container-fluid'>
+    <div class="row">
+        <div class="col-sm-8 bg-primary text-center pl-5 pr-5"></div>
+
+        <div class="col-sm-4 p-5">
+        <a href="#">
+            <div class="col-sm-12 bg-danger test text-center">
+                <h3>Séries suivis</h3>
+            </div>
+        </a>
+        <a href="#">
+            <div class="col-sm-12 bg-danger test text-center">
+                <h3>Séries à regarder plus tard</h3>
+            </div>
+        </a>
+        <a href="#">    
+            <div class="col-sm-12 bg-danger test text-center">
+                <h3>Mes vidéos</h3>
+            </div>
+        </a>    
+        </div>
+    </div>
+</div>
+
+<?php include ('../inc/footer.inc.php') ?>
