1d23922a2576138e0b53d2254f68963229e11eef
BalthPa <balthazarpapin@gmail.com>
2019-12-19 10:59:20 +0100

    Add header and footer

---
+++ pages/index.php
@@ -0,0 +1,11 @@
+<?php include ('../inc/header.inc.php') ?>
+
+<div class='container'>
+    <div class="row">
+        <div class="col-sm-8 bg-primary h-100"></div>
+        <div class="col-sm-4 bg-warning"></div>
+    </div>
+</div>
+
+<?php include ('../inc/footer.inc.php') ?>
+
