9cc689aabb2be497281df4c64a6bc3d5a798cb48
BalthPa <balthazarpapin@gmail.com>
2019-12-19 11:18:44 +0100

    Update index

--- pages/index.php
+++ pages/index.php
@@ -1,9 +1,23 @@
 <?php include ('../inc/header.inc.php') ?>
 
-<div class='container'>
+<div class='container-fluid'>
     <div class="row">
-        <div class="col-sm-8 bg-primary h-100"></div>
-        <div class="col-sm-4 bg-warning"></div>
+        <div class="col-sm-8 bg-primary text-center">
+            <h3>Fil d'actualité</h3>
+            <div class="col-sm-12 bg-secondary text-center m-5"></div>
+        </div>
+
+        <div class="col-sm-4">
+            <div class="col-sm-12 bg-danger test text-center">
+                <h3>Séries suivis</h3>
+            </div>
+            <div class="col-sm-12 bg-danger test text-center">
+                <h3>Séries à regarder plus tard</h3>
+            </div>
+            <div class="col-sm-12 bg-danger test text-center">
+                <h3>Mes vidéos</h3>
+            </div>
+        </div>
     </div>
 </div>
 
