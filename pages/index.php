91f84230b5646b4d53c053752fa7efb0797cd79c
BalthPa <balthazarpapin@gmail.com>
2019-12-19 12:10:43 +0100

    Add actuality

--- pages/index.php
+++ pages/index.php
@@ -2,21 +2,68 @@
 
 <div class='container-fluid'>
     <div class="row">
-        <div class="col-sm-8 bg-primary text-center">
-            <h3>Fil d'actualité</h3>
-            <div class="col-sm-12 bg-secondary text-center m-5"></div>
+        <div class="col-sm-8 bg-primary text-center pl-5 pr-5">
+            <div class="col-sm-12  text-center p-5">
+                <h3>Fil d'actualité</h3>
+            </div>
+            <div class="col-sm-12 bg-warning mb-5">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h3>John Doe</h3>
+                    </div>
+                    <div class="col-sm-6 bg-secondary">
+                        <p class="font-weight-bold">Ma nouvelle vidéo</p>
+                        <p>J'ai hate de vous la montrer !</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-12 bg-warning mt-5 mb-5">
+                <div class="row">
+                    <div class="col-sm-12 text-left">
+                        <h3 >Jane Doe</h3>
+                        <p>Trop bien la dernière vidéo de</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-12 bg-info mt-5 mb-5">
+                <div class="row">
+                    <div class="col-sm-12 text-left">
+                        <h3 >Jane Doe</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni delectus quae minima eius natus quidem amet consectetur, laborum provident accusamus deserunt vero ipsam in nisi maiores tempora, eum ipsum sapiente.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-12 bg-info mt-5 mb-5">
+                <div class="row">
+                    <div class="col-sm-12 text-left">
+                        <h3 >Jane Doe</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis repudiandae quae vero tempora libero molestias! Distinctio doloremque magni odit cupiditate explicabo. Sapiente, maiores possimus odio maxime exercitationem sint libero nobis!</p>
+                    </div>
+                </div>
+            </div>
+            
+
         </div>
 
-        <div class="col-sm-4">
+        <div class="col-sm-4 p-5">
+        <a href="#">
             <div class="col-sm-12 bg-danger test text-center">
                 <h3>Séries suivis</h3>
             </div>
+        </a>
+        <a href="#">
             <div class="col-sm-12 bg-danger test text-center">
                 <h3>Séries à regarder plus tard</h3>
             </div>
+        </a>
+        <a href="#">    
             <div class="col-sm-12 bg-danger test text-center">
                 <h3>Mes vidéos</h3>
             </div>
+        </a>    
         </div>
     </div>
 </div>
