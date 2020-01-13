0209bd500b281ce8b809c738707eb5472ffe1fb0
BalthPa <balthazarpapin@gmail.com>
2020-01-13 17:08:42 +0100

    Update

--- pages/index.php
+++ pages/index.php
@@ -1,67 +1,11 @@
 <?php include ('../inc/header.inc.php') ?>
 
-<div class='container-fluid'>
+<div class='container'>
     <div class="row">
-        <div class="actu col-sm-9 border border-info text-center pl-5 pr-5">
-
-            <div class="col-sm-12  text-center p-3">
-                <h3> Fil d'actualités </h3>
-            </div>
-
-            <div class="col-sm-12 bg-warning mb-5">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <h3> John Doe </h3>
-                    </div>
-                    <div class="col-sm-6 bg-secondary">
-                        <p class="font-weight-bold"> Ma nouvelle vidéo </p>
-                        <p> J'ai hâte de vous la montrer ! </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-12 bg-warning mt-5 mb-5">
-                <div class="row">
-                    <div class="col-sm-12 text-left">
-                        <h3 >Jane Doe</h3>
-                        <p>Trop bien la dernière vidéo de</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-12 bg-info mt-5 mb-5">
-                <div class="row">
-                    <div class="col-sm-12 text-left">
-                        <h3 >Jane Doe</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni delectus quae minima eius natus quidem amet consectetur, laborum provident accusamus deserunt vero ipsam in nisi maiores tempora, eum ipsum sapiente.</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-12 bg-info mt-5 mb-5">
-                <div class="row">
-                    <div class="col-sm-12 text-left">
-                        <h3 >Jane Doe</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis repudiandae quae vero tempora libero molestias! Distinctio doloremque magni odit cupiditate explicabo. Sapiente, maiores possimus odio maxime exercitationem sint libero nobis!</p>
-                    </div>
-                </div>
-            </div>
-            
-         </div>
-
-        <div class="right-side border border-success col-sm-3">
-
-            <div class="div-series col-sm-12 btn-group-vertical" role="group" aria-label="Basic example">
-                <button type="button" class="block-series btn btn-secondary col-sm-12"> Séries suivies </button>
-                <button type="button" class="block-series btn btn-secondary col-sm-12"> Séries à regarder plus tard </button>
-                <button type="button" class="block-series btn btn-secondary col-sm-12"> Mes vidéos </button>
-            </div>
-
-            <?php include ('../inc/footer.inc.php') ?>
-          
-        </div>
+        <div class="col-sm-8 bg-primary"></div>
+        <div class="col-sm-4 bg-warning"></div>
     </div>
 </div>
 
-
+<?php include ('../inc/footer.inc.php') ?>
 
