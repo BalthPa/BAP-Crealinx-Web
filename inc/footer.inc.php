5db24b0ae949eeddb1f39396d8e979d974c2ad01
BalthPa <balthazarpapin@gmail.com>
2020-01-14 16:56:24 +0100

    Fix Footer : Delete one part

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,41 +1,5 @@
 <div class="footer col-sm-12 border border-danger">
 
-    <div>
-        <h3 class="text-center"> Crealinx </h3>
-        <p class="text-center"> Plateforme de web-séries accessible pour tous </p>
-    </div>
-
-    
-    <div class="container text-center">
-        <div class="row">
-            <div class="col">
-            <img src="../img/mail.png" class="img-fluid" alt="Responsive image">
-            </div>
-            <div class="col order-12">
-            <img src="../img/twitter.png" class="img-fluid" alt="Responsive image">
-            </div>
-            <div class="col order-1">
-            <img src="../img/instagram.png" class="img-fluid" alt="Responsive image">
-            </div>
-            <div class="col">
-            <img src="../img/facebook.png" class="img-fluid" alt="Responsive image">
-            </div>
-        </div>
-    </div>
-
-
-    <h6 class="text-center"> © 2020 Crealinx </h6> 
-</div>
-
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
-
-
-
-<div class="footer col-sm-12 border border-danger">
-
     <div class="footer-parts">
         <h3 class="text-center"> Pop-corn Series </h3>
         <p> Plateforme communautaire de web-séries </p>
@@ -62,5 +26,9 @@
 
 
 
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
