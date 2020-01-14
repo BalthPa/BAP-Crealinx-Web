fb4d5e804ce4b362030d332a75a975fc764bc64f
ekypow <lucas.schrever@gmail.com>
2020-01-14 12:29:30 +0100

    Formulaire contact fonctionnel (connection.inc.php personnel)

--- inc/connection.inc.php
+++ inc/connection.inc.php
@@ -1,8 +1,15 @@
 <?php
-try{
-    $bdd = new PDO('mysql:host=127.0.0.1; port=8889 ;dbname=bap-crealinx', 'root', 'root');
+
+$db_host='mysql:dbname=bap-crealinx;host=localhost';
+$user = 'root';
+$pw = '';
+
+
+try {
+    $conn = new PDO($db_host, $user, $pw);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Échec lors de la connexion : ' . $e->getMessage();
 }
-catch(excpetion $e){
-    echo $e->getMessage();
-}
+
 ?>
